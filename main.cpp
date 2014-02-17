b41d45529358eacc4dbe015d348fbab17630bfcf
Christoph Malek <malekchristoph@gmail.com>
2014-02-17 19:31:21 +0100

    adapted to c++11 style

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
+	QApplication a{argc, argv};
 	main_window w;
 	w.show();
 
--- main_window.hpp
+++ main_window.hpp
@@ -1,22 +1,38 @@
 #ifndef MAIN_WINDOW_HPP
 #define MAIN_WINDOW_HPP
 
+
+#include "server_manager.hpp"
+#include "ui_main_window.h"
+
 #include <QMainWindow>
 
-namespace Ui {
-	class main_window;
-}
+
+
+namespace Ui
+{class main_window;}
 
 class main_window : public QMainWindow
 {
 	Q_OBJECT
 
+	Ui::main_window* m_ui;
+
+	server_manager m_servermgr;
+
 public:
-	explicit main_window(QWidget *parent = 0);
-	~main_window();
+	main_window(QWidget* parent = nullptr) :
+		QMainWindow{parent},
+		m_ui{new Ui::main_window}
+	{m_ui->setupUi(this);}
+
+	~main_window()
+	{delete m_ui;}
 
 private:
-	Ui::main_window *ui;
+
 };
+
+
 
 #endif // MAIN_WINDOW_HPP
