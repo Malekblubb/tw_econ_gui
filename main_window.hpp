ed0f865403e0c6f075484ebf1fa64ea5127fa276
Christoph Malek <malekchristoph@gmail.com>
2014-02-22 21:28:16 +0100

    added set_window_status

--- main_window.hpp
+++ main_window.hpp
@@ -33,6 +33,7 @@ public:
 		m_ui->setupUi(this);
 
 		connect(&m_servermgr, SIGNAL(log_added(const std::string&)), this, SLOT(_on_log_added(const std::string&)));
+		connect(&m_servermgr, SIGNAL(connection_timeout()), this, SLOT(_on_connection_timeout()));
 		connect(&m_servermgr, SIGNAL(connection_lost()), this, SLOT(_on_connection_lost()));
 		connect(&m_servermgr, SIGNAL(connection_start(const std::string&)), this, SLOT(_on_connection_start(const std::string&)));
 		connect(&m_servermgr, SIGNAL(logged_in()), this, SLOT(_on_login()));
@@ -45,8 +46,10 @@ private slots:
 	void on_btn_login_clicked()
 	{
 		m_servermgr.open_econ({m_ui->le_host->text().toStdString(), m_ui->sb_port->text().toStdString()}, m_ui->le_password->text().toStdString());
+		this->set_window_state("Trying to connect...");
 	}
 
+	// tab: main controlls
 	void on_btn_change_map_clicked()
 	{m_servermgr.exec_command("change_map " + m_ui->le_mapname->text().toStdString());}
 
@@ -80,6 +83,17 @@ private slots:
 	void on_btn_custom_cmd_send_clicked()
 	{m_servermgr.exec_command(m_ui->le_custom_cmd->text().toStdString());}
 
+
+	// tab: players
+	void on_btn_refresh_playerlist_clicked()
+	{m_servermgr.request_player_info();}
+
+	void on_btn_ban_ip_clicked()
+	{m_servermgr.exec_command("ban " + m_ui->le_ip_ban->text().toStdString() + " " + m_ui->le_ip_bantime->text().toStdString());}
+
+	void on_btn_unban_ip_clicked()
+	{m_servermgr.exec_command("unban " + m_ui->le_ip_ban->text().toStdString());}
+
 	void _on_log_added(const std::string& str)
 	{
 		m_ui->te_log->moveCursor(QTextCursor::End);
@@ -87,20 +101,32 @@ private slots:
 		m_ui->te_log->moveCursor(QTextCursor::End);
 	}
 
+	void _on_connection_timeout()
+	{
+		QMessageBox::critical(this, "Error", "Connection attempt timed out");
+		this->set_window_state("Connection failed");
+	}
+
 	void _on_connection_lost()
 	{
 		QMessageBox::critical(this, "Error", "Connection to server lost");
+		this->set_window_state("Connection lost");
 	}
 
 	void _on_connection_start(const std::string& addr_str)
 	{
 		m_ui->tw_main->setTabText(0, addr_str.c_str());
+		this->set_window_state("Connected");
 	}
 
 	void _on_login()
 	{
 		m_ui->tw_main->setTabText(0, m_ui->tw_main->tabText(0) + "(logged)");
 	}
+
+private:
+	void set_window_state(const std::string& state)
+	{m_ui->lb_state->setText(state.c_str());}
 };
 
 
