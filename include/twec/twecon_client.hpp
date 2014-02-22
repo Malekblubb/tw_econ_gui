afb56669295d95ef931dbbf6feec3829c16a2f61
Christoph Malek <malekchristoph@gmail.com>
2014-02-22 21:29:02 +0100

    added on_connection_timeout

--- include/twec/twecon_client.hpp
+++ include/twec/twecon_client.hpp
@@ -7,14 +7,18 @@
 #define TWEC_TWECON_CLIENT_HPP
 
 
+#include "data_parser.hpp"
 #include "telnet_client.hpp"
+
+#include <mlk/time/simple_timer.h>
 
 
 namespace twec
 {
 	class twecon_client
 	{
-		static constexpr std::size_t m_maxrecv{1024};
+		static constexpr std::size_t m_maxrecv{2048};
+		static constexpr mlk::ullong m_timeout{4000};
 		telnet_client m_telnet;
 
 		std::stringstream m_log;
@@ -23,13 +27,19 @@ namespace twec
 		mlk::ntw::ip_address m_next_addr;
 		std::string m_next_passwd;
 
-		bool m_valid{false}, m_need_connect{false}, m_connected{false}, m_need_send_pass{false}, m_logged{false}, m_need_send_connection_lost{true};
+		mlk::tm::simple_timer m_conntimeout_timer{m_timeout};
+
+		bool m_valid{false}, m_connected{false}, m_logged{false};
+		bool m_need_connect{false}, m_need_send_pass{false}, m_need_send_connection_lost{true};
+		bool m_playerinfo_requested{false};
 
 	public:
 		mlk::slot<const std::string&> on_log_added;
+		mlk::slot<> on_connection_timeout;
 		mlk::slot<> on_connection_lost;
 		mlk::slot<> on_connection;
 		mlk::slot<> on_login;
+		mlk::slot<> on_playerinfo_received;
 
 		twecon_client() :
 			m_recvdata(m_maxrecv)
@@ -39,8 +49,17 @@ namespace twec
 		{
 			// connect when needed
 			if(m_need_connect)
-			{
-				if(!m_connected) this->try_connect();
+			{				
+				if(!m_connected)
+				{
+					if(m_conntimeout_timer.timed_out())
+					{
+						m_need_connect = false;
+						on_connection_timeout();
+					}
+					else this->try_connect();
+				}
+
 				else this->on_internal_connection();
 			}
 
@@ -62,6 +81,7 @@ namespace twec
 					{
 						on_connection_lost();
 						m_need_send_connection_lost = false;
+						this->on_internal_connection_lost();
 					}
 					return;
 				}
@@ -74,8 +94,17 @@ namespace twec
 			if(len > 0)
 			{
 				auto str(this->make_string(m_recvdata, len));
-				if(!m_logged && mlk::stl_string::contains("Authentication successful. External console access granted.", str))
+				data_parser dp{str};
+
+
+				if(!m_logged && dp.is_login_reply())
 					this->on_internal_login();
+				if(m_playerinfo_requested && dp.is_player_reply())
+				{
+					on_playerinfo_received();
+					m_playerinfo_requested = false;
+				}
+
 				this->add_to_log(str);
 			}
 		}
@@ -87,12 +116,19 @@ namespace twec
 			m_connected = false;
 			m_logged = false;
 			m_valid = false;
+			m_conntimeout_timer.run();
 		}
 
 		void login(const std::string& pass)
 		{
 			m_need_send_pass = true;
 			m_next_passwd = pass;
+		}
+
+		void request_player_info()
+		{
+			m_playerinfo_requested = true;
+			this->exec_command("status");
 		}
 
 		ssize_t exec_command(const std::string& command) const
@@ -131,6 +167,15 @@ namespace twec
 		{
 			m_logged = true;
 			on_login();
+		}
+
+		void on_internal_connection_lost() noexcept
+		{
+			m_connected = false;
+			m_logged = false;
+			m_valid = false;
+			m_need_connect = false;
+			m_need_send_pass = false;
 		}
 
 		void try_connect()
